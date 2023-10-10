0c30e61d56f482ee2e67117568969aaf7acf7155
unknown <lukang11@icloud.com>
2023-10-10 19:43:36 +0200

    Adding react router plus initialization of profile page

--- fun-finder-frontend/src/App.js
+++ fun-finder-frontend/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProfilePage from "./Components/ProfilePage/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header></Header>
+        <Routes>
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
