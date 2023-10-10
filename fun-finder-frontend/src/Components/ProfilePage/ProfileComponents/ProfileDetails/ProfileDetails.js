351685c726b5e80ed6d9edafc45ae0fb75862b9b
unknown <lukang11@icloud.com>
2023-10-10 21:00:30 +0200

    Creation of Profile description with css and profile details also with css

--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileDetails/ProfileDetails.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileDetails/ProfileDetails.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "../ProfileDetails/ProfileDetails.css";
+import monkey from "../../../../Images/Monkey_test.jpg";
 
 const ProfileDetails = () => {
-  return <div>ProfileDetails</div>;
+  return (
+    <div className="profile-page-wrapper">
+      <div className="profile-page-image"></div>
+      <div className="profile-page-image-cont">
+        <div className="profile-desc-item">
+          <div className="desc-item">81</div>
+          <div className="desc-item">Punkty</div>
+        </div>
+        <div className="profile-desc-item">
+          {" "}
+          <div className="desc-item">32</div>
+          <div className="desc-item">Obserwujących</div>
+        </div>
+        <div className="profile-desc-item">
+          {" "}
+          <div className="desc-item">51</div>
+          <div className="desc-item">Obserwatorów</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileDetails;
