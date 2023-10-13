c321930a1c853c19790735d70674957c169d31c7
unknown <lukang11@icloud.com>
2023-10-13 21:38:05 +0200

    Resolving mistakes with css connected to header , Implementeing components that was missing in previous commit

--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileEvents/ProfileEvents.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileEvents/ProfileEvents.js
@@ -1,7 +1,78 @@
 import React from "react";
+import "./ProfileEvents.css";
 
 const ProfileEvents = () => {
-  return <div>ProfileEvents</div>;
+  const test_data = [
+    {
+      id: "1",
+      date: "2023-10-12",
+      nazwa_wydarzenia: "Przykładowe Wydarzenie",
+      liczba: 32,
+    },
+    {
+      id: "2",
+      date: "2023-09-13",
+      nazwa_wydarzenia: "Przykładowe Wydarzenie",
+      liczba: 12,
+    },
+    {
+      id: "3",
+      date: "2023-11-13",
+      nazwa_wydarzenia: "Przykładowe Wydarzenie",
+      liczba: 22,
+    },
+    {
+      id: "4",
+      date: "2023-12-13",
+      nazwa_wydarzenia: "Przykładowe Wydarzenie",
+      liczba: 2,
+    },
+    {
+      id: "5",
+      date: "2023-07-13",
+      nazwa_wydarzenia: "Przykładowe Wydarzenie",
+      liczba: 97,
+    },
+  ];
+
+  return (
+    <div>
+      <div className="events-card">
+        <h2 className="">Wydarzenia</h2>
+        <ul>
+          <div className="events-row">
+            {" "}
+            <li className="events-item-icon style-list-elem text-color">
+              AKTYWNOŚĆ
+            </li>
+            <li className="events-item-data style-list-elem text-color">
+              DATA
+            </li>
+            <li className="events-item-name style-list-elem text-color">
+              WYDARZENIE
+            </li>
+            <li className="events-item style-list-elem text-color">OSOBY</li>
+            <li className="events-item style-list-elem text-color">id</li>
+          </div>
+          {test_data.map((element, index) => (
+            <li key={element.id} className="events-row bottom-border">
+              <div className="events-item-icon style-list-elem">test_icon</div>
+              <div className="events-item-data style-list-elem">
+                {element.date}
+              </div>
+              <div className="events-item-name style-list-elem">
+                {element.nazwa_wydarzenia}
+              </div>
+              <div className="events-item style-list-elem">
+                {element.liczba}
+              </div>
+              <div className="events-item style-list-elem">{element.id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileEvents;
--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileHobbies/ProfileHobbies.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileHobbies/ProfileHobbies.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "./ProfileHobbies.css";
 
 const ProfileHobbies = () => {
-  return <div>ProfileHobbies</div>;
+  const data = [
+    { id: 1, hobby: "KINO" },
+    { id: 2, hobby: "PRZYGODA" },
+    { id: 3, hobby: "PUB" },
+    { id: 4, hobby: "TEATR" },
+    { id: 5, hobby: "MALARSTWO" },
+    { id: 6, hobby: "SZTUKA" },
+    { id: 7, hobby: "JEDZENIE" },
+    { id: 8, hobby: "SPORT" },
+    { id: 9, hobby: "SZTUKA" },
+    { id: 10, hobby: "WINO" },
+    { id: 11, hobby: "PIWO" },
+    { id: 12, hobby: "GRY PLANSZOWE" },
+  ];
+  return (
+    <div className="hobbies-card">
+      <div className="hobbies-first-row">
+        <h2>Zainteresowania</h2>
+        <div className="hobbies-add-button">+</div>
+      </div>
+      <div className="hobbies-second-row">
+        <div className="hobbies-contener-wrapper">
+          {data.map((element) => (
+            <div key={element.id} className="hobbies-contener-wrapper-item">
+              <div>{element.id}</div>
+              <div>{element.hobby}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileHobbies;
