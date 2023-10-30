75a8176620376c95d765c27b4c4f803fdb4fd0ce
MaciejDubowik <maciejxpoland@gmail.com>
2023-10-30 17:45:47 +0100

    section 1 completed

--- fun-finder-frontend/src/Components/WelcomePage/WelcomePage.js
+++ fun-finder-frontend/src/Components/WelcomePage/WelcomePage.js
@@ -1,11 +1,41 @@
 import React from "react";
-import "./WelcomePage.css"
+import "./WelcomePage.css";
+import firstSectionImg from "./Images/first-section-img.png";
+import Button from 'react-bootstrap/Button';
 
-function WelcomePage(){
+
+function WelcomePage() {
     return (
-    <div>
-        <h2>hello world!</h2>
-    </div>
+        <div>
+            <div className="first-section">
+                <div className="container">
+                    <div className="left">
+                        <div className="section-title">Poznawaj ludzi z<br></br> twoimi zainteresowaniami</div>
+                        <div className="section-text">Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est. Nullam quis risus justo. Curabitur luctus sed elit ac vehicula. Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. </div>
+
+                        <div className="buttons-container">
+                            <div className="left">
+                                <div className="section-filled-btn ">
+                                    Stwórz konto
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div className="section-outline-btn">
+                                    Dowiedz się więcej
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="right">
+                        <img src={firstSectionImg} className="fullscreenImg" />
+                    </div>
+                </div>
+            </div>
+            <div className="second-section">
+                <h2>hello world!</h2>
+            </div>
+        </div>
     );
 }
 
