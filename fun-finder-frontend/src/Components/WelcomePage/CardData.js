1fd965e85ea846767eda0b23d2036b226313b2ab
MaciejDubowik <maciejxpoland@gmail.com>
2023-10-31 21:10:16 +0100

    second-section completed

---
+++ fun-finder-frontend/src/Components/WelcomePage/CardData.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        title: "Podaj swoje dane",
+        text: "Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est. Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est."
+    },
+    {
+        id: 2,
+        title: "Wybierz zainteresowania",
+        text: "Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est. Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est."
+    },
+    {
+        id: 3,
+        title: "Przedstaw się",
+        text: "Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est. Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est."
+    }
+]
--- fun-finder-frontend/src/Components/WelcomePage/WelcomePage.js
+++ fun-finder-frontend/src/Components/WelcomePage/WelcomePage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./WelcomePage.css";
 import firstSectionImg from "./Images/first-section-img.png";
-import Button from 'react-bootstrap/Button';
-
+import Card from "./Card.js";
+import data from "./CardData.js";
 
 function WelcomePage() {
+
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+
+            />
+        )
+    })
+
+
     return (
         <div>
             <div className="first-section">
@@ -32,8 +44,13 @@ function WelcomePage() {
                     </div>
                 </div>
             </div>
+
             <div className="second-section">
-                <h2>hello world!</h2>
+                <div className="section-title">Tworzenie profilu. Szybkie i proste</div>
+                <div className="section-text">Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. Duis luctus feugiat tellus non ultrices. Nullam eget iaculis leo. Mauris et tellus est. Nullam quis risus justo. Curabitur luctus sed elit ac vehicula. Dolor sit amet, consectetur adipiscing elit. Fusce hendrerit tincidunt libero ut tempor. </div>
+
+                <div className="cards-container">{cards}</div>
+
             </div>
         </div>
     );
