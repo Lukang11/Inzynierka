6ad8ab79a694e4d07623f214eec8da354c1673f9
Hubert Konkel <77440455+SirHubix@users.noreply.github.com>
2023-11-07 18:02:33 +0100

    init

--- fun-finder-frontend/src/App.js
+++ fun-finder-frontend/src/App.js
@@ -5,6 +5,7 @@ import ProfilePage from "./Components/ProfilePage/ProfilePage";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import ChatPage from "./Components/ChatPage/ChatPage";
+import EventsPage from "./Components/Events/EventsPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/chat" element={<ChatPage />} />
+          <Route path="/events" element={<EventsPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ fun-finder-frontend/src/Components/Events/EventsPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./UsersEvents.css";
+import UsersEvents from "./UsersEvents";
+
+
+
+function eventsPage() {
+    return (
+        <div className="eventsPage">
+            <div className="usersEventsSection">
+                <UsersEvents/>
+            </div>
+            </div>
+    );
+}
+
+
+
+export default eventsPage;
