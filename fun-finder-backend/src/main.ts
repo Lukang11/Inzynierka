a79c015ca9557de48aa5aa875ffc0bff3ca0fb4d
sebciulina <sgransicki@gmail.com>
2023-11-30 22:56:19 +0100

    added cors to main.ts and implemented sending data from the form to the server

--- fun-finder-backend/src/main.ts
+++ fun-finder-backend/src/main.ts
@@ -3,9 +3,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(7000);
 }
 
