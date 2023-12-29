8dfcf850a08b7a1b79acd3987a30de30bc00b8e9
sebciulina <sgransicki@gmail.com>
2023-12-30 00:43:07 +0100

    event battler participants css fix and active user icon

--- fun-finder-frontend/src/Components/EventBattler/EventBattlerParticipants/EventBattlerParticipants.js
+++ fun-finder-frontend/src/Components/EventBattler/EventBattlerParticipants/EventBattlerParticipants.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./EventBattlerParticipants.css";
+import ActiveCircle from "../../ActiveCircle/ActiveCircle";
 
 function EventBattlerParticipants() {
 
@@ -16,6 +17,12 @@ function EventBattlerParticipants() {
       lname: 'seba',
       avatar: 'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
     },
+    {
+      email: 'nie@jeadadawdwdawdaddwdawdawdawawddawwadawdawdawdawdawdawdachleba.pl',
+      fname: 'seba2',
+      lname: 'seba2',
+      avatar: 'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
+    },
   ];
 
   return (
@@ -24,6 +31,11 @@ function EventBattlerParticipants() {
       {users.map((user, index) => (
         <li key={index}>
           <div className="event-battler-participants-user-details">
+          <div className="event-battler-participants-activeCircle">
+           <ActiveCircle isActive={true} /> 
+           
+          </div>
+          
             <img src={user.avatar} alt="Avatar" />
             <a>{user.email}<br /></a>
             <a>{user.fname} {user.lname}</a>
