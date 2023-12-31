ba78c49c9acacef6afec081932d1f49ef05b85f2
SirHubix <77440455+SirHubix@users.noreply.github.com>
2023-12-31 01:05:21 +0100

    getting location from google maps,small form fixes

--- fun-finder-frontend/src/Components/Events/CreateEvent.js
+++ fun-finder-frontend/src/Components/Events/CreateEvent.js
@@ -4,24 +4,34 @@ import './CreateEvent.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../../Utils/AuthProvider"; 
-import { GoogleMap, Marker } from '@react-google-maps/api';
+import { GoogleMap, Marker,LoadScript } from '@react-google-maps/api';
+
+
 
 function CreateEvent() {
   const { user } = useAuth(); // do pobierania id uz
 
   const [name, setName] = useState('');
-  const [time, setTime] = useState('');
   const [people, setPeople] = useState('');
   const [category, setCategory] = useState('');
-  const [date, setDate] = useState(''); 
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [latLng, setLatLng] = useState({ lat: 0, lng: 0 });
   const [map, setMap] = useState(null);
+  const apiKey= process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
-  const onMapClick = (event) => {
-    setLatLng({
+  const [clickedLatLng, setClickedLatLng] = useState(null);
+
+  const mapClickHandler = (event) => {
+    setClickedLatLng({
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
     });
+    setLatLng(clickedLatLng);
+  };
+  const mapContainerStyle = {
+    width: '100%',
+    height: '450px',
   };
 
   const defaultCenter = {
@@ -54,8 +64,6 @@ function CreateEvent() {
   const handleSubmit = (event) => {
     event.preventDefault();
     createNewEventChat(user._id,name);
-    console.log(`Event Name: ${name}, Event Time: ${time}, Number of People: ${people}, Category: ${category}, Event Date: ${date}`);
-    alert('Wydarzenie zostało utworzone'); 
     navigate('/events');
   };
 
@@ -68,18 +76,31 @@ function CreateEvent() {
         <div className='label-text'>Nazwa:</div>
         <input type="text" className='input-textbox' value={name} onChange={e => setName(e.target.value)} />
         <div className='label-text'>Data rozpoczęcia:</div>
-        <input type="text" className='input-textbox' value={time} onChange={e => setTime(e.target.value)} />
+        <input type="datetime-local" className='input-textbox' value={startDate} onChange={e => setStartDate(e.target.value)} />
         <div className='label-text'>Data zakończenia</div>
-        <input type="text" className='input-textbox' value={time} onChange={e => setTime(e.target.value)} />
+        <input type="datetime-local" className='input-textbox' value={endDate} onChange={e => setEndDate(e.target.value)} />
         <div className='label-text'>Ilość osób:</div>
-        <input type="text" className='input-textbox' value={date} onChange={e => setPeople(e.target.value)} />
+        <input type="text" className='input-textbox' value={people} onChange={e => setPeople(e.target.value)} />
         <div className='label-text'>Opis wydarzenia:</div>
         <textarea id="input-textbox-2" name="input-textbox-2" rows="6" cols="40"> </textarea>
         
       </div>
       <div className='events-card'>
         <div className='info-header'>Lokalizacja</div>
-        <iframe className='events-map' src="https://www.google.com/maps/embed?pb=..." width="" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <LoadScript googleMapsApiKey={apiKey}>
+          <div style={mapContainerStyle}>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              center={defaultCenter}
+              zoom={10}
+              onClick={mapClickHandler}
+              onLoad={onLoad}
+            >
+                {clickedLatLng && <Marker position={{ lat: clickedLatLng.lat, lng: clickedLatLng.lng }} />}
+
+            </GoogleMap>
+          </div>
+        </LoadScript>
         <div className='photo-display'>Tu będzie dodawane zdjęcie ale jeszcze nie ma serwisu</div>
       </div>
       <div className='events-card'>
