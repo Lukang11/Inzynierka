455759c5ce4756d2c814ecf2f3714d2606d47b78
MaciejDubowik <maciejxpoland@gmail.com>
2024-01-02 13:31:18 +0100

    fixed incorrectly scaled background in Modals

--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/EditProfileModal/EditProfileModal.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/EditProfileModal/EditProfileModal.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 
-
 function EditProfileModal({ onClick }) {
-   
-
     return (
-        <div className="hobbies-modal-wrapper">
-            <div className="hobbies-wrapper-form">
-                <div className="close-modal" onClick={() => { onClick()}}>
+        <div className="hobbies-modal-wrapper" style={{ background: "white" }}>
+            <div className="hobbies-wrapper-form" style={{ maxWidth: "600px", margin: "0 auto" }}>
+                <div className="close-modal" onClick={() => onClick()}>
                     X
                 </div>
-        </div>
+                {/* Add your modal content here */}
+            </div>
         </div>
     );
 }
