2f57a94be3f1431d0dcedec997aaba6744ce3691
MaciejDubowik <maciejxpoland@gmail.com>
2024-01-03 02:03:13 +0100

    profile edit modal frontend

--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/EditProfileModal/EditProfileModal.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/EditProfileModal/EditProfileModal.js
@@ -1,15 +1,58 @@
 import React, { useState, useEffect } from "react";
+import "./EditProfileModalCss.css";
 
 function EditProfileModal({ onClick }) {
+    const [description, setDescription] = useState("");
+    const [profilePhoto, setProfilePhoto] = useState(null);
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value);
+    };
+
+    const handleProfilePhotoChange = (e) => {
+        setProfilePhoto(e.target.files[0]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    };
+
+    useEffect(() => {
+        // Apply overflow: hidden; to body when the modal is open
+        document.body.style.overflow = "hidden";
+        // Cleanup function to remove the style when the component unmounts
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, []); 
+    
     return (
-        <div className="hobbies-modal-wrapper" style={{ background: "white" }}>
-            <div className="hobbies-wrapper-form" style={{ maxWidth: "600px", margin: "0 auto" }}>
-                <div className="close-modal" onClick={() => onClick()}>
-                    X
-                </div>
-                {/* Add your modal content here */}
+        
+        <div className="profile-modal-wrapper">
+            <div className="close-profile-modal" onClick={() => onClick()}>
+                X
+            </div>
+            <div className="profile-form">
+
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <div className="profile-modal-text">Description:</div>
+                        <div className="profile-description">
+                            <input id="description" value={description} onChange={handleDescriptionChange} />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="profile-modal-text">Profile Photo:</div>
+                        <div>
+                            <input type="file" id="profilePhoto" onChange={handleProfilePhotoChange} />
+                        </div>
+                    </div>
+                    <button className="profile-save-btn" type="submit">Save</button>
+                </form>
             </div>
         </div>
+          
     );
 }
 
