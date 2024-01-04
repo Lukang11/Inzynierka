3fc0a6c06cc80a95a6a54170487a17d35870d2b3
MaciejDubowik <maciejxpoland@gmail.com>
2024-01-04 17:39:39 +0100

    display hobbies icons

--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileHobbies/ProfileHobbies.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/ProfileHobbies/ProfileHobbies.js
@@ -3,54 +3,67 @@ import "./ProfileHobbies.css";
 import { useAuth } from "../../../../Utils/AuthProvider";
 import axios from "axios";
 import HobbiesModal from "../HobbiesModal/HobbiesModal";
+import { hobbiesData } from "../../../../Data/HobbiesData";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProfileHobbies = () => {
-  const [hobbiesData, sethobbiesData] = useState();
+  const [hobbies, sethobbies] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const url = "http://localhost:7000/events/";
   const { user } = useAuth();
+
   useEffect(() => {
     const fetchData = async () => {
       if (user && user.email) {
         try {
-          const hobbies = await axios.get(`${url}hobbies/${user.email}`);
-          const newHobbies = Array.from(new Set(hobbies.data))
-          sethobbiesData(() => newHobbies);
+          const hobbiesResponse = await axios.get(`${url}hobbies/${user.email}`);
+          const userHobbies = Array.from(new Set(hobbiesResponse.data));
+          sethobbies(() => userHobbies);
         } catch (error) {
           console.error("Error fetching hobbies:", error);
         }
       }
     };
+
     fetchData();
   }, [user]);
-  function onclick() {
+
+  function onClick() {
     setIsOpen((val) => !val);
-    
+  }
+
+  function getHobbyIcon(hobbyName) {
+    const matchingHobby = hobbiesData.find(hobby => hobby.name === hobbyName);
+    const matchingIcon = matchingHobby ? matchingHobby.icon : null;
+    return matchingIcon;
   }
 
   return (
     <div className="hobbies-card">
       <div className="hobbies-first-row">
         <h2>Zainteresowania</h2>
-        {console.log(isOpen)}
-        <div className="hobbies-add-button" onClick={() => onclick()}>
+        <div className="hobbies-add-button" onClick={onClick}>
           +
         </div>
       </div>
       <div className="hobbies-second-row">
         <div className="hobbies-contener-wrapper">
-          {hobbiesData
-            ? hobbiesData.map((element, index) => (
+          {hobbies
+            ? hobbies.map((element, index) => (
               <div key={index} className="hobbies-contener-wrapper-item">
-                <div>{index}</div>
+                <div className="hobby-icon">
+                  {getHobbyIcon(element) && (
+                    <FontAwesomeIcon icon={getHobbyIcon(element)} />
+                  )}
+                </div>
                 <div>{element}</div>
               </div>
             ))
             : null}
         </div>
       </div>
-    
-      {isOpen ? <HobbiesModal onClick={onclick} /> : null}
+
+      {isOpen ? <HobbiesModal onClick={onClick} /> : null}
     </div>
   );
 };
