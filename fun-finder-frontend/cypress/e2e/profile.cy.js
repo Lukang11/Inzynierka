106a9f0a6163aa9a4e81f237391333ce282dbfb8
unknown <lukang11@icloud.com>
2024-01-25 02:36:49 +0100

    Removal of redundant empty spaces

--- fun-finder-frontend/cypress/e2e/profile.cy.js
+++ fun-finder-frontend/cypress/e2e/profile.cy.js
@@ -1,26 +1,24 @@
-describe('Profile functionality', () => {
-  it('User should see his profile after login', () => {
-    cy.loginUser('1234@gmail.com', 'P@ssw0rd');
+describe("Profile functionality", () => {
+  it("User should see his profile after login", () => {
+    cy.loginUser("1234@gmail.com", "P@ssw0rd");
     cy.loadProfile();
-
-
-  })
-  it('User should edit his profile', () => {
-  cy.loginUser('1234@gmail.com', 'P@ssw0rd');
-  cy.loadProfile();
-  cy.get('.edit-profile-btn').click();
-  cy.get('#description').type('Test description');
-  cy.get('.profile-save-btn').click();
-  cy.get('.desc-cont').should('contain', 'Test description');
-  cy.get('.hobbies-add-button').click();
-  cy.get('.hobbies-item-wrapper > :nth-child(3)').click();
-  cy.get('.hobbies-item-wrapper > :nth-child(8)').click();
-  cy.get('.modal-send-btn').click();
-  cy.get('.close-modal').click();
-  cy.get('.hobbies-contener-wrapper').should('contain', 'Gotowanie','Motoryzacja');
-  
-
-
-  })
-
-})
+  });
+  it("User should edit his profile", () => {
+    cy.loginUser("1234@gmail.com", "P@ssw0rd");
+    cy.loadProfile();
+    cy.get(".edit-profile-btn").click();
+    cy.get("#description").type("Test description");
+    cy.get(".profile-save-btn").click();
+    cy.get(".desc-cont").should("contain", "Test description");
+    cy.get(".hobbies-add-button").click();
+    cy.get(".hobbies-item-wrapper > :nth-child(3)").click();
+    cy.get(".hobbies-item-wrapper > :nth-child(8)").click();
+    cy.get(".modal-send-btn").click();
+    cy.get(".close-modal").click();
+    cy.get(".hobbies-contener-wrapper").should(
+      "contain",
+      "Gotowanie",
+      "Motoryzacja"
+    );
+  });
+});
