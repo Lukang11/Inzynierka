39b62b560edbf424b41d15d08ea1afc1db3c6896
MaciejDubowik <maciejxpoland@gmail.com>
2024-01-27 19:44:37 +0100

    create search box and some styling

--- fun-finder-frontend/src/Components/SearchPage/SearchPage.js
+++ fun-finder-frontend/src/Components/SearchPage/SearchPage.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./SearchPage.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {    faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchPage() {
+    const handleSubmit = (e) => e.preventDefault()
+
+    const handleSearchChange = (e) => {
+        console.log("szukam")
+    }
+
     return(
-    <div className="">
-        <h1>Wyszukaj</h1>
-        <p>Znajdź znajomych</p>
+    <div className="search-page-container ">
+        <div className="search-container">
+            <h1 className="search-header">Lepsze połączenia, bliższe relacje </h1>
+            <p>Wyszukiwanie przyjaciół jeszcze nigdy nie było tak ekscytujące</p>
+            <p>Poznawaj ludzi z FunFinder!</p>
+
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input
+                    className="search_input"
+                    type="text"
+                    id="search"
+                    onChange={handleSearchChange}
+                />
+                <button className="search_btn">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </form>
+        </div>
+
     </div>
     )
 }
