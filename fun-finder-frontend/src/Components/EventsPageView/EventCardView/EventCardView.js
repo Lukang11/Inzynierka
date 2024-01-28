f60a80769fe76407b5f1bf4dba9d28f968172a10
SirHubix <77440455+SirHubix@users.noreply.github.com>
2024-01-29 00:54:03 +0100

    Updated EventCardView

--- fun-finder-frontend/src/Components/EventsPageView/EventCardView/EventCardView.js
+++ fun-finder-frontend/src/Components/EventsPageView/EventCardView/EventCardView.js
@@ -6,6 +6,22 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 function EventCardView({ eventInfo, places }) {
   const navigate = useNavigate();
+  console.log(eventInfo)
+
+  const splitDateTime = (dateTime) => {
+    if (!dateTime) return { date: '', time: '' };
+
+    const [date, time] = dateTime.split('T');
+    const formattedDate = date.split('-').reverse().join('.');
+    const formattedTime = time.substring(0, 5);
+
+    return { date: formattedDate, time: formattedTime };
+  };
+
+ 
+  const { date, time } = splitDateTime(eventInfo.eventStart);
+  console.log({date, time});
+
   const handleRowClick = (id) => {
     navigate(`/event-info/${id}`);
   };
@@ -19,7 +35,6 @@ function EventCardView({ eventInfo, places }) {
         <div className="events-view-image"></div>
         <div className="events-view-description">
           <div className="events-view-card-item-spec-left">
-            {" "}
             <div className="events-info-card-name">
               {places ? eventInfo.displayName.text : eventInfo.name}
             </div>
@@ -28,17 +43,18 @@ function EventCardView({ eventInfo, places }) {
             </div>
           </div>
           <div className="events-view-card-item-spec-right">
-            {" "}
-            <div className="events-view-card-hour">20:00</div>
-            <div className="events-view-card-date">22.12.2023</div>
+            <div className="events-view-card-hour">{time}</div>
+            <div className="events-view-card-date">{date}</div>
             <div>
-              {places ? (
-                <FontAwesomeIcon
-                  icon={faStar}
-                  style={{ color: "rgb(232, 198, 26)" }}
-                />
-              ) : null}
-              {places ? ` ${eventInfo.rating}` : null}
+              {places && (
+                <>
+                  <FontAwesomeIcon
+                    icon={faStar}
+                    style={{ color: "rgb(232, 198, 26)" }}
+                  />
+                  {` ${eventInfo.rating}`}
+                </>
+              )}
             </div>
           </div>
         </div>
