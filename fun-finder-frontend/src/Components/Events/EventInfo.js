ba39f3b695c9591f5bcfe511f4781b60a60c7caf
sebciulina <sgransicki@gmail.com>
2024-01-30 02:51:06 +0100

    EventInfo hotfix

--- fun-finder-frontend/src/Components/Events/EventInfo.js
+++ fun-finder-frontend/src/Components/Events/EventInfo.js
@@ -18,7 +18,7 @@ export const EventInfo = () => {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   function refreshPage() {
     window.location.reload(false);
-}
+  }
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -77,95 +77,95 @@ export const EventInfo = () => {
 
   const addUserToEventChat = async () => {
     await axios.post('http://localhost:7000/clouds/event/addUserToChat',
-    {
-      userId: user._id,
-      eventId: eventId
-      
-    })
+      {
+        userId: user._id,
+        eventId: eventId
 
-  const handleAddEventToUser = async () => {
-    const eventDataBody = {
-      eventId: eventData._id,
-      name: eventData.name,
-      eventDescription: eventData.eventDescription,
-      eventStart: eventData.eventStart,
-      eventEnd: eventData.eventEnd,
-      location: eventData.location,
-      eventPhoto:  eventData.eventPhoto
+      })
     }
-    const addEventToUser = await fetch(`http://localhost:7000/users/add-event/${user.email}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(eventDataBody),
-    });
-
-    if(!addEventToUser.ok){
-      throw new Error(`Błąd HTTP! Status: ${addEventToUser.status}`);
-    }
-  }
-
-  const handleRegisterEvent = async () => {
-    // refreshPage();
-    if (eventData && eventData.users && eventData.users.includes(user.email))  {
-      setIsUserRegistered(true);
-      alert('Jesteś już zarejestrowany na to wydarzenie!');
-      return;
-    }
-  
-    try {
-      const userEmail = user.email;
-  
-      const response = await fetch(`http://localhost:7000/events/add/user`, {
+    const handleAddEventToUser = async () => {
+      const eventDataBody = {
+        eventId: eventData._id,
+        name: eventData.name,
+        eventDescription: eventData.eventDescription,
+        eventStart: eventData.eventStart,
+        eventEnd: eventData.eventEnd,
+        location: eventData.location,
+        eventPhoto: eventData.eventPhoto
+      }
+      const addEventToUser = await fetch(`http://localhost:7000/users/add-event/${user.email}`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
-
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ eventId: eventId, userEmail: userEmail }),
-        
+        body: JSON.stringify(eventDataBody),
       });
-  
-      if (!response.ok) {
-        throw new Error(`Błąd HTTP! Status: ${response.status}`);
-      }
-      // trzeba tu dodać jeszcze bad response do tego jak bedzie max uzytkowników
-      addUserToEventChat();
-      console.log(response.status);
-      const result = await response.json();
-      setIsUserRegistered(true); 
-      alert('Pomyślnie zarejestrowano na wydarzenie!');
-    } catch (error) {
-      console.error('Błąd rejestracji na wydarzenie:', error)
-    }
-  };
-  
-  const handleClick = async () => {
-    await handleRegisterEvent();
-    await handleAddEventToUser();
-  };
 
-  return (
-    <div className="event-info-container">
-      {eventData ? (
-        <>
-          <div className="overlap">
-            
-            <div className="event-name">{eventData.name}</div>
-          <div className="event-address">{eventData.location}</div>
-          <p className="event-information">{eventData.eventDescription}</p>
-          <button className={`register-on-event-button ${isUserRegistered ? 'register-on-event-button-disabled' : ''}`} onClick= {handleClick} disabled={isUserRegistered}>
-              {isUserRegistered ? 'Jesteś już zapisany na to wydarzenie' : 'Zapisz się na wydarzenie'}
-            </button>
-          </div>
-          <div className="event-map-container">
-          <p className="event-enrolled-users">
-            <span className="enrolled-users">Zapisanych uczestników: </span>
-            <span className="event-number-of-users">{eventData.eventParticipantsEmail.length}</span>
-          </p>
+      if (!addEventToUser.ok) {
+        throw new Error(`Błąd HTTP! Status: ${addEventToUser.status}`);
+      }
+    }
+
+    const handleRegisterEvent = async () => {
+      // refreshPage();
+      if (eventData && eventData.users && eventData.users.includes(user.email)) {
+        setIsUserRegistered(true);
+        alert('Jesteś już zarejestrowany na to wydarzenie!');
+        return;
+      }
+
+      try {
+        const userEmail = user.email;
+
+        const response = await fetch(`http://localhost:7000/events/add/user`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+
+          },
+          body: JSON.stringify({ eventId: eventId, userEmail: userEmail }),
+
+        });
+
+        if (!response.ok) {
+          throw new Error(`Błąd HTTP! Status: ${response.status}`);
+        }
+        // trzeba tu dodać jeszcze bad response do tego jak bedzie max uzytkowników
+        addUserToEventChat();
+        console.log(response.status);
+        // const result = await response.json();
+        setIsUserRegistered(true);
+        alert('Pomyślnie zarejestrowano na wydarzenie!');
+      } catch (error) {
+        console.error('Błąd rejestracji na wydarzenie:', error)
+      }
+    };
+
+    const handleClick = async () => {
+      await handleRegisterEvent();
+      await handleAddEventToUser();
+    };
+
+    return (
+      <div className="event-info-container">
+        {eventData ? (
+          <>
+            <div className="overlap">
+
+              <div className="event-name">{eventData.name}</div>
+              <div className="event-address">{eventData.location}</div>
+              <p className="event-information">{eventData.eventDescription}</p>
+              <button className={`register-on-event-button ${isUserRegistered ? 'register-on-event-button-disabled' : ''}`} onClick={handleClick} disabled={isUserRegistered}>
+                {isUserRegistered ? 'Jesteś już zapisany na to wydarzenie' : 'Zapisz się na wydarzenie'}
+              </button>
+            </div>
+            <div className="event-map-container">
+              <p className="event-enrolled-users">
+                <span className="enrolled-users">Zapisanych uczestników: </span>
+                <span className="event-number-of-users">{eventData.eventParticipantsEmail.length}</span>
+              </p>
               {latitude !== null && longitude !== null && (
-                <GoogleMap 
+                <GoogleMap
                   mapContainerClassName="event-map"
                   className="event-map"
                   center={{ lat: latitude, lng: longitude }}
@@ -174,14 +174,14 @@ export const EventInfo = () => {
                   <Marker position={{ lat: latitude, lng: longitude }} />
                 </GoogleMap>
               )}
-            
-          </div>
-        </>
-      ) : (
-        <p>Ładowanie danych...</p>
-      )}
-    </div>
-  );
-};
+
+            </div>
+          </>
+        ) : (
+          <p>Ładowanie danych...</p>
+        )}
+      </div>
+    );
+  };
 
 export default EventInfo;
