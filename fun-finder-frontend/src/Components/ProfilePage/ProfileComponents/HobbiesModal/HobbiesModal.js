a4b931455b4dc641a8b57adba18d8d9b3da6ce67
unknown <lukang11@icloud.com>
2024-01-31 17:06:23 +0100

    Adding icon to hobbies in profile hobbies component

--- fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/HobbiesModal/HobbiesModal.js
+++ fun-finder-frontend/src/Components/ProfilePage/ProfileComponents/HobbiesModal/HobbiesModal.js
@@ -4,17 +4,21 @@ import HobbiesItem from "./HobbiesItem/HobbiesItem";
 import axios from "axios";
 import { useAuth } from "../../../../Utils/AuthProvider";
 import { hobbiesData } from "../../../../Data/HobbiesData.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function HobbiesModal({ onClick }) {
+function HobbiesModal({ onClick, getIcon }) {
   const [hobbies, setHobbies] = useState([]);
   const { user } = useAuth();
 
   const [relatedHobbies, setRelatedHobbies] = useState(hobbiesData);
 
   const addItem = (item) => {
-    const correctItem = hobbiesData.find((hobby) => hobby.name === item)
+    const correctItem = hobbiesData.find((hobby) => hobby.name === item);
 
-    setHobbies((prevHobbies) => [...prevHobbies, { name: correctItem.name, data: correctItem.data }]);
+    setHobbies((prevHobbies) => [
+      ...prevHobbies,
+      { name: correctItem.name, data: correctItem.data },
+    ]);
   };
 
   const removeItem = (index) => {
@@ -32,17 +36,14 @@ function HobbiesModal({ onClick }) {
     });
   };
 
-
   const addRelatedItem = (item) => {
     setRelatedHobbies((prevHobbies) => {
       const hobbyData = hobbiesData.find((hobby) => hobby.name === item);
       const updatedState = [...prevHobbies, hobbyData];
-      
+
       return updatedState;
     });
   };
-
-
 
   function refreshPage() {
     window.location.reload(false);
@@ -52,14 +53,17 @@ function HobbiesModal({ onClick }) {
       if (user && user.email) {
         try {
           let url = "http://localhost:7000/users/";
-          const hobbiesTags = hobbies.map(hobby => hobby.data);
+          const hobbiesTags = hobbies.map((hobby) => hobby.data);
           const hobbiesTagsResult = [].concat(...hobbiesTags);
-          console.log(hobbiesTagsResult)
+          console.log(hobbiesTagsResult);
 
-          const hobbiesNames = hobbies.map(hobby => hobby.name);
+          const hobbiesNames = hobbies.map((hobby) => hobby.name);
           const hobbiesNamesResult = [].concat(...hobbiesNames);
 
-          await axios.post(`${url}update-user-hobbies/${user.email}`, { hobbies: hobbiesTagsResult, hobbiesName: hobbiesNamesResult });
+          await axios.post(`${url}update-user-hobbies/${user.email}`, {
+            hobbies: hobbiesTagsResult,
+            hobbiesName: hobbiesNamesResult,
+          });
         } catch (error) {
           console.error("Error fetching hobbies:", error);
         }
@@ -69,14 +73,18 @@ function HobbiesModal({ onClick }) {
     fetchData();
   };
 
-  useEffect(() => {
-
-  }, [hobbies, relatedHobbies]);
+  useEffect(() => {}, [hobbies, relatedHobbies]);
 
   return (
     <div className="hobbies-modal-wrapper">
       <div className="hobbies-wrapper-form">
-        <div className="close-modal" onClick={() => { onClick(); refreshPage() }}>
+        <div
+          className="close-modal"
+          onClick={() => {
+            onClick();
+            refreshPage();
+          }}
+        >
           X
         </div>
         <div className="hobbies-form">
@@ -91,12 +99,14 @@ function HobbiesModal({ onClick }) {
                     <div
                       key={index}
                       onClick={() => {
-
                         addRelatedItem(val.name);
                         removeItem(index);
                       }}
                       className="hobbies-item"
                     >
+                      <div className="hobbies-prf-icon">
+                        <FontAwesomeIcon icon={getIcon(val.name)} />
+                      </div>
                       {val.name}
                     </div>
                   );
@@ -111,9 +121,9 @@ function HobbiesModal({ onClick }) {
             <div className="hobbies-items">
               <h3>Dostępne kategorie:</h3>
               <div className="hobbies-item-wrapper">
-              {relatedHobbies.map((value, index) => (
+                {relatedHobbies.map((value, index) => (
                   <HobbiesItem
-                    key={index} 
+                    key={index}
                     name={value.name}
                     icon={value.icon}
                     onClick={addItem}
